ddd4824e6fa60194c40022d511954ac02ce94fb8
John Behman <behmanjohn@gmail.com>
2023-06-12 23:44:28 -0500

    FixingDataBase

--- Models/CustomerOrder.cs
+++ Models/CustomerOrder.cs
@@ -7,30 +7,30 @@ namespace Cake_Supplies.Models
     public class CustomerOrder
     {
 
-        public int Id { get; set; }
+        public int OrderId { get; set; }
         public int CustomerId { get; set; }
         public DateTime PickUpDate { get; set; }
         public string? CustomerName { get; set; }
         public string? Address { get; set; }
-        public string Phone { get; set; }
+        public string? Phone { get; set; }
         public string? Email { get; set; }
-        public string ImageUrl { get; set; }
-        public int Quantity { get; set; }
-        public string Name { get; set; }
-        public AllOrder OrderItems { get; set; }
+        public string? ImageUrl { get; set; }
+        public int? Quantity { get; set; }
+        public string? ItemName { get; set; }
+        //public Orders OrderItems { get; set; }
 
     }
 
-    public class AllOrder
-    {
+    //public class Orders
+    //{
 
-         public int Id { get; set; }
-        public int OrderId { get; set; }
-        public int ItemId { get; set; }
-        public int Quantity { get; set; }
-        // public List<Items> Items { get; set; }
+    //     public int Id { get; set; }
+    //    public int OrderId { get; set; }
+    //    public int ItemId { get; set; }
+    //    public int Quantity { get; set; }
+    //    // public List<Items> Items { get; set; }
 
 
-    }
+    //}
 }
   
--- Models/Order.cs
+++ Models/Order.cs
@@ -5,10 +5,7 @@
         public int Id { get; set; }
         public int CustomerId { get; set; }
         public DateTime PickUpDate { get; set; } 
-        public string? Name { get; set; }
-        public string? Address { get; set; }
-        public string Phone { get; set; }
-        public string? Email { get; set; }
+      
     }
 }
 
